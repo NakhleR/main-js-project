fef8e0a83fffa387f550e1cb1e66356e04f3ae3a
NakhleR <nakhler2k2@gmail.com>
2023-03-11 12:51:35 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,39 +1,50 @@
 const computerPlay = () => {
-  const items = ['Rock', 'Paper', 'Scissor'];
+  const items = ['Rock', 'Paper', 'Scissors'];
 
   return (items[Math.floor(Math.random() * items.length)]);
 }
 
-
+let playerScore = 0;
+let computerScore = 0;
 
 const playRound = (player, computer) => {
 
   let playerPick = player.toUpperCase();
   let computerPick = computer.toUpperCase();
 
+  console.log(`Player chose ${playerPick}`);
+  console.log(`Computer chose ${computerPick}`);
   if (playerPick === computerPick) {
     return "It's a tie !";
   }
 
-  if (
-    (player === "rock" && computer === "scissors") ||
-    (player === "paper" && computer === "rock") ||
-    (player === "scissors" && computer === "paper")
-  ) {
+  if ((playerPick == "ROCK" && computerPick == "SCISSORS") || (playerPick == "PAPER" && computerPick == "ROCK") ||
+    (playerPick == "SCISSORS" && computerPick == "PAPER")) {
+    playerScore++;
     return `You win! ${player} beats ${computer}.`;
+  } else {
+    computerScore++;
+    return `You lose! ${computer} beats ${player}.`;
   }
-
-  return `You lose! ${computer} beats ${player}.`;
 }
+
 
 const game = () => {
   for (let i = 0; i < 5; i++) {
-    const player = prompt("Rock,Paper,Scissor");
+    const player = prompt("Rock,Paper,Scissors");
     const computer = computerPlay();
+
+    console.log(`=======ROUND ${i + 1}=======`);
+
     const result = playRound(player, computer);
 
     console.log(result);
+    console.log(`Computer Score: ${computerScore}`);
+    console.log(`Player Score: ${playerScore}`);
   }
 }
 
 game();
+
+console.log(computerScore);
+console.log(playerScore);
